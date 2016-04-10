8244e8f4d0ffe6abd148a23af07e6b4239e1bf92
Nicholas Miller <nicholas.anthony.miller@gmail.com>
2016-04-10 21:47:32 +0700

    twitter iteration 3 step 2

--- 03_ruby/03c_ruby_web_project/micro_blogger.rb
+++ 03_ruby/03c_ruby_web_project/micro_blogger.rb
@@ -11,7 +11,31 @@ class MicroBlogger
     def tweet(message)
         @client.update(message)
     end
+
+    def dm(target, message)
+        puts "Trying to send #{target} this direct message: "
+        puts message
+        message = "d @#{target} #{message}"
+        tweet(message)
+    end
+
+    def run
+        puts "Welcome to the JSL Twitter Client!"
+        command = ""
+        while command != "q"
+            printf "enter command: "
+            input = gets.chomp
+            parts = input.split(" ")
+            command = parts[0]
+            case command
+                when 'q' then puts "Goodbye!"
+                when 't' then tweet(parts[1..-1].join(" "))
+                when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+                else puts "I'm sorry, Dave, I'm afraid I can't #{command.downcase}."
+            end
+        end
+    end
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("Hello, world!")
+blogger.run
